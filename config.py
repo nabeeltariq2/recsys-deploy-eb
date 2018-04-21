8242a08f21e1e0aae163d563479ae979cb262a75
Nabeel Tariq <nabeeltariq2@gmail.com>
2018-04-20 23:23:15 -0400

    load files

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+from sqlalchemy import *
+
+DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://nabeeltariq2:nabeeltariq2@aa1jxwiqzsxn6zd.cpliemudyxqt.us-east-1.rds.amazonaws.com/ebdb?charset=utf8')
+
+# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://nabeeltariq2:nabeeltariq2@flaskdb.cpliemudyxqt.us-east-1.rds.amazonaws.com/firstdb?charset=utf8')
+
+# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://flask:nabeeltariq2@flasktest.cpliemudyxqt.us-east-1.rds.amazonaws.com/flaskdb?charset=utf8')
+
+
+# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:sesame@localhost/amazonrecsys?charset=utf8')
+
+# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:sesame@localhost/amazonmusicalins2?charset=utf8')
+
+
+
+
+# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://flask:nabeeltariq2@aa1bus1tdvnpq5m.cpliemudyxqt.us-east-1.rds.amazonaws.com/ebdb?charset=utf8')
+
+#new database
+# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://nabeeltariq2:nabeeltariq2@aa14nod91ksnt0i.cpliemudyxqt.us-east-1.rds.amazonaws.com/ebdb?charset=utf8')
